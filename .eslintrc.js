27a00404571f38074a592cff70cc768402bf8dd1
Carlos Rivas <rivascarlosva@gmail.com>
2022-02-06 15:32:47 -0600

    Config eslint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,11 @@ module.exports = {
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
-      },
-    },
-  ],
+  rules: {
+    'no-shadow': 'off',
+    'no-undef': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+    'react-native/no-inline-styles': 'off',
+  },
+  ignorePatterns: ['*.js'] // Only config js files
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,5 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
+  tabWidth: 4,
   singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
+  printWidth: 100
 };
